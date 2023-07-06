3569f54e50f8fd03ce49adb2be5e46a01ecff652
RBarrios <rodolfo.9408@gmail.com>
2023-07-05 22:59:11 -0500

    feat: add splash screen

--- app/src/main/java/com/skgtecnologia/sisem/ui/Navigation.kt
+++ app/src/main/java/com/skgtecnologia/sisem/ui/Navigation.kt
@@ -1,6 +1,7 @@
 package com.skgtecnologia.sisem.ui
 
 object NavigationRoute {
+    const val SPLASH = "splash"
     const val FETCH = "fetch"
     const val MY_SCREEN_UI = "my_screen_ui"
 }
---
+++ app/src/main/java/com/skgtecnologia/sisem/ui/splash/SplashScreen.kt
@@ -0,0 +1,46 @@
+package com.skgtecnologia.sisem.ui.splash
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.skgtecnologia.sisem.R
+import kotlinx.coroutines.delay
+
+@Composable
+fun SplashScreen(
+        modifier: Modifier = Modifier,
+        onClick: () -> Unit
+) {
+    Box(modifier = modifier.fillMaxSize()) {
+        Image(
+                modifier = Modifier.fillMaxSize(),
+                painter = painterResource(id = R.drawable.ic_background),
+                contentDescription = null,
+                contentScale = ContentScale.Crop
+        )
+        Image(
+                modifier = Modifier.align(Alignment.Center),
+                painter = painterResource(id = R.drawable.ic_icon_splash),
+                contentDescription = "logo sisem"
+        )
+    }
+
+    LaunchedEffect(key1 = true) {
+        delay(1000)
+        onClick()
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun SplashScreenPreview() {
+    SplashScreen {}
+}
