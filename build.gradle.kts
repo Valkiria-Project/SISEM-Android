a7dba41fa557fbc1df11776d6bec0c91665d1361
Juan Camilo Villada <jvillad1@gmail.com>
2023-07-06 01:41:42 -0500

    chore: Update golden values (#2)

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,25 @@ plugins {
     id("com.android.library") version "8.0.2" apply false
     id("org.jetbrains.kotlin.android") version "1.8.21" apply false
     id("com.google.dagger.hilt.android") version "2.44.2" apply false
-    id("org.jlleitschuh.gradle.ktlint") version "11.0.0" apply false
-    id("io.gitlab.arturbosch.detekt") version "1.17.0" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "11.1.0" apply true
+    id("io.gitlab.arturbosch.detekt") version "1.17.0" apply true
+    id("com.github.ben-manes.versions") version "0.42.0" apply true
+}
+
+apply(from = "buildscripts/githooks.gradle")
+apply(from = "buildscripts/setup.gradle")
+
+subprojects {
+    apply(from = "../buildscripts/ktlint.gradle")
+    apply(from = "../buildscripts/detekt.gradle")
+    apply(from = "../buildscripts/versionsplugin.gradle")
+}
+
+task("clean") {
+    delete(rootProject.buildDir)
+}
+
+afterEvaluate {
+    // We install the hook at the first occasion
+    tasks["clean"].dependsOn("installGitHooks")
 }
