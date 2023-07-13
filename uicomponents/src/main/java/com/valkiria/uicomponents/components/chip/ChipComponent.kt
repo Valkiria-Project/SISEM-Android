7e353a3f7a0aa0151b35f325136b14e769f2e783
Juan Vilada <jvillad1@gmail.com>
2023-07-13 09:31:30 -0500

    fixed: Chip style (WIP)

--- uicomponents/src/main/java/com/valkiria/uicomponents/components/chip/ChipComponent.kt
+++ uicomponents/src/main/java/com/valkiria/uicomponents/components/chip/ChipComponent.kt
@@ -1,6 +1,7 @@
 package com.valkiria.uicomponents.components.chip
 
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AssistChip
 import androidx.compose.material3.AssistChipDefaults
 import androidx.compose.material3.Icon
@@ -10,7 +11,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.valkiria.uicomponents.R
 import com.valkiria.uicomponents.props.ChipStyleUi
 import com.valkiria.uicomponents.props.TextStyleUi
@@ -20,23 +24,30 @@ fun ChipComponent(
     model: ChipUiModel,
     modifier: Modifier = Modifier
 ) {
-    model.text?.let {
+    model.text?.let { chipLabel ->
         AssistChip(
             onClick = { },
-            label = { Text(text = it) },
+            label = { // FIXME: Create function to extract the TextStyle from the input
+                Text(
+                    text = chipLabel,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.W700
+                )
+            },
             leadingIcon = {
                 Icon(
                     painter = painterResource(id = R.drawable.ic_ambulancia), // FIXME: Create function to extract the ic from the input
                     contentDescription = "recurso test",
-                    modifier = modifier.size(AssistChipDefaults.IconSize)
+                    modifier = modifier.size(40.dp)
                 )
             },
-            colors = AssistChipDefaults.assistChipColors(
+            shape = RoundedCornerShape(25.dp),
+            colors = AssistChipDefaults.assistChipColors( // FIXME: Create function to extract the style
                 containerColor = MaterialTheme.colorScheme.primary,
                 labelColor = Color.Black,
                 leadingIconContentColor = Color.Black
             ),
-            border = AssistChipDefaults.assistChipBorder(
+            border = AssistChipDefaults.assistChipBorder( // FIXME: Create function to extract the style
                 borderColor = MaterialTheme.colorScheme.primary
             )
         )
