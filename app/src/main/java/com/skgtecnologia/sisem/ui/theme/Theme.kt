66c5d26cd235de93146d4d1a6e57ce0276364ce1
Juan Vilada <jvillad1@gmail.com>
2023-07-14 10:45:17 -0500

    fixed: com.valkiria.uicomponents.theme.Typography in Theme

--- app/src/main/java/com/skgtecnologia/sisem/ui/theme/Theme.kt
+++ app/src/main/java/com/skgtecnologia/sisem/ui/theme/Theme.kt
@@ -7,10 +7,10 @@ import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
+import com.valkiria.uicomponents.theme.Typography
 
 private val DarkColorScheme = darkColorScheme(
     primary = DodgerBlue,
