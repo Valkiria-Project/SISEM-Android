80b181830afbc8f51f86dfa8814e456d71ba7a7a
Juan Vilada <jvillad1@gmail.com>
2023-07-13 21:37:02 -0500

    fixed: Update package for request models

--- app/src/main/java/com/skgtecnologia/sisem/data/login/remote/api/LoginApi.kt
+++ app/src/main/java/com/skgtecnologia/sisem/data/login/remote/api/LoginApi.kt
@@ -1,6 +1,6 @@
 package com.skgtecnologia.sisem.data.login.remote.api
 
-import com.skgtecnologia.sisem.data.login.remote.model.LoginScreenBody
+import com.skgtecnologia.sisem.data.model.bodyrequest.ScreenBody
 import com.skgtecnologia.sisem.data.login.remote.model.LoginScreenResponse
 import retrofit2.Response
 import retrofit2.http.Body
@@ -9,6 +9,6 @@ import retrofit2.http.POST
 interface LoginApi {
 
     @POST("sisem-api/v1/screen/login")
-    suspend fun getLoginScreen(@Body loginScreenBody: LoginScreenBody) :
+    suspend fun getLoginScreen(@Body screenBody: ScreenBody) :
         Response<LoginScreenResponse>
 }
