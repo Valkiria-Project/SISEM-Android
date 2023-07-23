1a8933df3c9e1c2f20671f32b7f7c0998afa5af9
Juan Vilada <jvillad1@gmail.com>
2023-07-23 17:26:53 -0500

    feat: Handle errorBody() for network requests gracefully and replace api calls throughout the app

--- app/src/main/java/com/skgtecnologia/sisem/data/auth/remote/AuthRemoteDataSource.kt
+++ app/src/main/java/com/skgtecnologia/sisem/data/auth/remote/AuthRemoteDataSource.kt
@@ -1,55 +1,25 @@
 package com.skgtecnologia.sisem.data.auth.remote
 
-import com.skgtecnologia.sisem.commons.extensions.recoverResult
-import com.skgtecnologia.sisem.commons.extensions.resultOf
+import com.skgtecnologia.sisem.commons.extensions.mapResult
 import com.skgtecnologia.sisem.data.auth.remote.model.AuthenticateBody
 import com.skgtecnologia.sisem.data.auth.remote.model.mapToDomain
-import com.skgtecnologia.sisem.data.remote.extensions.handleThrowable
-import com.skgtecnologia.sisem.data.remote.extensions.toError
-import com.skgtecnologia.sisem.data.remote.model.error.mapToDomain
+import com.skgtecnologia.sisem.data.remote.extensions.apiCall
 import com.skgtecnologia.sisem.domain.login.model.AccessTokenModel
 import javax.inject.Inject
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import timber.log.Timber
 
 class AuthRemoteDataSource @Inject constructor(
     private val authApi: AuthApi
 ) {
 
     suspend fun authenticate(username: String, password: String): Result<AccessTokenModel> =
-//        apiCall {
-//            authApi.authenticate(
-//                authenticateBody = AuthenticateBody(
-//                    username = username,
-//                    password = password
-//                )
-//            )
-//        }.mapResult {
-//            it.mapToDomain()
-//        }
-        resultOf {
-            val response = withContext(Dispatchers.IO) {
-                authApi.authenticate(
-                    authenticateBody = AuthenticateBody(
-                        username = username,
-                        password = password
-                    )
+        apiCall {
+            authApi.authenticate(
+                authenticateBody = AuthenticateBody(
+                    username = username,
+                    password = password
                 )
-            }
-
-            val body = response.body()
-
-            if (response.isSuccessful && body != null) {
-                body.mapToDomain()
-            } else {
-                Timber.wtf("The retrieved response is not successful and/or body is empty")
-                val errorType = response.errorBody().toError()
-                errorType?.mapToDomain()?.let {
-                    throw it
-                } ?: error("The retrieved response is not successful and/or body is empty")
-            }
-        }.recoverResult {
-            throw it.handleThrowable()
+            )
+        }.mapResult {
+            it.mapToDomain()
         }
 }
--- app/src/main/java/com/skgtecnologia/sisem/data/deviceauth/remote/DeviceAuthRemoteDataSource.kt
+++ app/src/main/java/com/skgtecnologia/sisem/data/deviceauth/remote/DeviceAuthRemoteDataSource.kt
@@ -1,63 +1,43 @@
 package com.skgtecnologia.sisem.data.deviceauth.remote
 
-import com.skgtecnologia.sisem.commons.extensions.resultOf
+import com.skgtecnologia.sisem.commons.extensions.mapResult
 import com.skgtecnologia.sisem.data.deviceauth.remote.model.AssociateDeviceBody
 import com.skgtecnologia.sisem.data.deviceauth.remote.model.mapToDomain
+import com.skgtecnologia.sisem.data.remote.extensions.apiCall
 import com.skgtecnologia.sisem.data.remote.model.screen.Params
 import com.skgtecnologia.sisem.data.remote.model.screen.ScreenBody
 import com.skgtecnologia.sisem.data.remote.model.screen.mapToDomain
 import com.skgtecnologia.sisem.domain.deviceauth.model.AssociateDeviceModel
 import com.skgtecnologia.sisem.domain.model.screen.ScreenModel
 import javax.inject.Inject
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import timber.log.Timber
 
 class DeviceAuthRemoteDataSource @Inject constructor(
     private val deviceAuthApi: DeviceAuthApi
 ) {
 
-    suspend fun getDeviceAuthScreen(serial: String): Result<ScreenModel> = resultOf {
-        val response = withContext(Dispatchers.IO) {
-            deviceAuthApi.getDeviceAuthScreen(
-                screenBody = ScreenBody(
-                    params = Params(serial = serial)
-                )
+    suspend fun getDeviceAuthScreen(serial: String): Result<ScreenModel> = apiCall {
+        deviceAuthApi.getDeviceAuthScreen(
+            screenBody = ScreenBody(
+                params = Params(serial = serial)
             )
-        }
-
-        val body = response.body()
-
-        if (response.isSuccessful && body != null) {
-            body.mapToDomain()
-        } else {
-            Timber.wtf("The retrieved response is not successful and/or body is empty")
-            error("The retrieved response is not successful and/or body is empty")
-        }
+        )
+    }.mapResult {
+        it.mapToDomain()
     }
 
     suspend fun associateDevice(
         licensePlate: String,
         serial: String,
         code: String
-    ): Result<AssociateDeviceModel> = resultOf {
-        val response = withContext(Dispatchers.IO) {
-            deviceAuthApi.associateDevice(
-                associateDeviceBody = AssociateDeviceBody(
-                    licensePlate = licensePlate,
-                    serial = serial,
-                    code = code
-                )
+    ): Result<AssociateDeviceModel> = apiCall {
+        deviceAuthApi.associateDevice(
+            associateDeviceBody = AssociateDeviceBody(
+                licensePlate = licensePlate,
+                serial = serial,
+                code = code
             )
-        }
-
-        val body = response.body()
-
-        if (response.isSuccessful && body != null) {
-            body.mapToDomain()
-        } else {
-            Timber.wtf("The retrieved response is not successful and/or body is empty")
-            error("The retrieved response is not successful and/or body is empty")
-        }
+        )
+    }.mapResult {
+        it.mapToDomain()
     }
 }
