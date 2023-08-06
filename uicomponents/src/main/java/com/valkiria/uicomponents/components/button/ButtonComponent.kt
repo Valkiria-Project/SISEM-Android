2a75d773371e2ac22a3d866483313fe3d3674808
Juan Vilada <jvillad1@gmail.com>
2023-08-06 11:21:39 -0500

    feat: Add Logic for PreOperational new components (WIP)

--- uicomponents/src/main/java/com/valkiria/uicomponents/components/button/ButtonComponent.kt
+++ uicomponents/src/main/java/com/valkiria/uicomponents/components/button/ButtonComponent.kt
@@ -13,7 +13,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.valkiria.uicomponents.mocks.getLoginButtonUiModel
 import com.valkiria.uicomponents.mocks.getLoginForgotButtonUiModel
 import com.valkiria.uicomponents.props.ButtonSize
