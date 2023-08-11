579de27e6f46fcd927fcde4976962299401b84aa
Juan Vilada <jvillad1@gmail.com>
2023-08-10 19:01:10 -0500

    chore: Make CI happy?

--- app/src/main/java/com/skgtecnologia/sisem/domain/model/body/ContentHeaderModel.kt
+++ app/src/main/java/com/skgtecnologia/sisem/domain/model/body/ContentHeaderModel.kt
@@ -3,7 +3,6 @@ package com.skgtecnologia.sisem.domain.model.body
 import androidx.compose.ui.Modifier
 import com.skgtecnologia.sisem.domain.model.header.HeaderModel
 import com.skgtecnologia.sisem.domain.model.header.TextModel
-import com.valkiria.uicomponents.props.TextStyle
 
 data class ContentHeaderModel(
     val identifier: String,
