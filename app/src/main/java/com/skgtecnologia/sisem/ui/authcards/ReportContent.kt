2e860eaaea27d4c6af1708fdb1318fabfe87ac3e
Juan Vilada <jvillad1@gmail.com>
2023-08-12 09:32:01 -0500

    chore: Make CI happy?

--- app/src/main/java/com/skgtecnologia/sisem/ui/authcards/ReportContent.kt
+++ app/src/main/java/com/skgtecnologia/sisem/ui/authcards/ReportContent.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.PagerScope
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
