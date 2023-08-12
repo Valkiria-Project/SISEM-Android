d926d4c2593bbab496ee2da5717873810392c7ec
Juan Vilada <jvillad1@gmail.com>
2023-08-12 10:49:20 -0500

    chore: change jvmTarget from 17 to 1.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,11 +36,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "1.8"
         freeCompilerArgs += listOf(
             "-opt-in=androidx.compose.foundation.ExperimentalFoundationApi",
             "-opt-in=androidx.compose.ui.ExperimentalComposeUiApi",
