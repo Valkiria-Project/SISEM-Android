edbeaedb9b1f6c454f2fc17505ba6aa33db11491
Juan Vilada <jvillad1@gmail.com>
2023-08-12 20:08:59 -0500

    chore: migrate to libs.versions.toml (WIP)

--- uicomponents/build.gradle.kts
+++ uicomponents/build.gradle.kts
@@ -48,44 +48,40 @@ android {
 dependencies {
 
     // Android
-    implementation("androidx.activity:activity-compose:1.7.2")
-    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation(libs.activity.compose)
+    implementation(libs.androidx.constraintlayout.compose)
+    implementation(libs.material)
 
     // Compose
-    implementation(platform("androidx.compose:compose-bom:2023.06.01"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
+    implementation(platform(libs.compose.bom))
+    implementation(libs.ui)
+    implementation(libs.ui.graphics)
     implementation("androidx.compose.ui:ui-text-google-fonts")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.material:material")
-    implementation("androidx.compose.material:material-icons-extended")
+    implementation(libs.ui.tooling.preview)
+    implementation(libs.material3)
 
     // Logging
-    implementation("com.jakewharton.timber:timber:5.0.1")
+    implementation(libs.timber)
 
     // Media
-    implementation("com.google.accompanist:accompanist-placeholder-material:0.30.1")
-    implementation("io.coil-kt:coil-compose:2.4.0")
-    implementation("com.airbnb.android:lottie-compose:6.1.0")
-    implementation("com.github.ireward:compose-html:1.0.2")
+    implementation(libs.accompanist.placeholder.material)
+    implementation(libs.coil.compose)
+    implementation(libs.compose.html)
 
     // Maps
-    implementation("com.mapbox.maps:android:10.14.0")
+    implementation(libs.mapbox.android)
 
     // Unit Testing
-    testImplementation("junit:junit:4.13.2")
+    testImplementation(libs.junit)
 
     // UI Testing
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.06.01"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    androidTestImplementation(platform(libs.compose.bom))
+    androidTestImplementation(libs.ui.test.junit4)
 
     // Debug
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
-    debugImplementation("com.squareup.leakcanary:leakcanary-android:2.12")
+    debugImplementation(libs.ui.tooling)
+    debugImplementation(libs.ui.test.manifest)
+    debugImplementation(libs.leakcanary.android)
 }
