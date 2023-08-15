da4872e55dcb777489031e48127c9d4a571d6f2d
Juan Camilo Villada <jvillad1@gmail.com>
2023-08-15 17:17:12 -0500

    Update app/src/main/java/com/skgtecnologia/sisem/data/changepassword/remote/model/ChangePasswordResponse.kt

--- app/src/main/java/com/skgtecnologia/sisem/data/changepassword/remote/model/ChangePasswordResponse.kt
+++ app/src/main/java/com/skgtecnologia/sisem/data/changepassword/remote/model/ChangePasswordResponse.kt
@@ -4,5 +4,5 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class ChangePasswordResponse(
-    val message: String
+    @Json(name = "message") val message: String
 )
