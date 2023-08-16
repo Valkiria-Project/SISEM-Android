1601c5866104c1b66374ebf695d98a55667dd455
RBarrios <rodolfo.9408@gmail.com>
2023-08-16 15:11:20 -0500

    fix: make ci happy

--- uicomponents/src/main/java/com/valkiria/uicomponents/components/button/ButtonComponent.kt
+++ uicomponents/src/main/java/com/valkiria/uicomponents/components/button/ButtonComponent.kt
@@ -1,7 +1,6 @@
 package com.valkiria.uicomponents.components.button
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
