f8acff9ccb7a6ab6149611f27ff01f92fc83d4be
Juan Vilada <jvillad1@gmail.com>
2023-08-27 18:28:41 -0500

    feat: Update BASE_URL

--- app/src/main/java/com/skgtecnologia/sisem/di/auth/AuthNetworkModule.kt
+++ app/src/main/java/com/skgtecnologia/sisem/di/auth/AuthNetworkModule.kt
@@ -18,7 +18,7 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
-private const val BASE_URL = "//http://34.139.125.3:80/sisem-api/"
+private const val BASE_URL = "http://34.139.125.3:80/sisem-api/"
 
 @Module(includes = [CoreNetworkModule::class])
 @InstallIn(SingletonComponent::class)
