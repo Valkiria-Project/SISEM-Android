f1862906e8b4c3e1e3f39a1d2ac305ae2ea10826
Juan Vilada <jvillad1@gmail.com>
2023-08-31 03:22:15 -0500

    chore: Make CI happy?

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
 
     defaultConfig {
         applicationId = "com.skgtecnologia.sisem"
-        minSdk = 24
+        minSdk = 31
         targetSdk = 34
         versionCode = 1
         versionName = "1.0.0"
