d5d296250498f31a3280c50f6a38608107d70595
Juan Vilada <jvillad1@gmail.com>
2023-09-02 21:19:48 -0500

    feat: Add logic for CameraScreen (WIP)

--- app/src/main/java/com/skgtecnologia/sisem/ui/imageselection/ImageSelectionUiState.kt
+++
@@ -1,8 +0,0 @@
-package com.skgtecnologia.sisem.ui.imageselection
-
-data class ImageSelectionUiState(
-    val onGoBack: Boolean = false,
-    val onTakePicture: Boolean = false,
-    val onImagesConfirmed: Boolean = false,
-    val onImagesCancelled: Boolean = false
-)
