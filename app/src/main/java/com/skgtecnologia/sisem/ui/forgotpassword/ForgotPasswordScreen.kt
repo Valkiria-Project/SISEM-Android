a6fe094af1dfba55245debb078d38dc545a1db37
Juan Vilada <jvillad1@gmail.com>
2023-09-17 17:13:47 -0500

    fix: ic_heartbeat.xml and create ForgotPasswordScreen (WIP)

---
+++ app/src/main/java/com/skgtecnologia/sisem/ui/forgotpassword/ForgotPasswordScreen.kt
@@ -0,0 +1,14 @@
+package com.skgtecnologia.sisem.ui.forgotpassword
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.skgtecnologia.sisem.ui.navigation.model.NavigationModel
+
+@Suppress("LongMethod")
+@Composable
+fun ForgotPasswordScreen(
+    isTablet: Boolean,
+    modifier: Modifier = Modifier,
+    onNavigation: (loginNavigationModel: NavigationModel?) -> Unit
+) {
+}
