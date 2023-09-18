3de05eeec714544401bc8a74d1f022fb47697785
Juan Camilo Villada <jvillad1@gmail.com>
2023-09-17 21:02:05 -0500

    chore: make CI happy?

--- uicomponents/src/main/java/com/valkiria/uicomponents/bricks/chip/FilterChipView.kt
+++ uicomponents/src/main/java/com/valkiria/uicomponents/bricks/chip/FilterChipView.kt
@@ -7,10 +7,6 @@ import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
