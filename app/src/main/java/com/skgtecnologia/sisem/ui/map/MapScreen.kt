7bdeaa20cc14bec8b170d96187d81a323238e9fc
Juan Vilada <jvillad1@gmail.com>
2023-09-19 07:11:28 -0500

    fix: add MapScreen with MenuDrawer icon style as Figma

--- app/src/main/java/com/skgtecnologia/sisem/ui/map/MapScreen.kt
+++ app/src/main/java/com/skgtecnologia/sisem/ui/map/MapScreen.kt
@@ -1,7 +1,10 @@
 package com.skgtecnologia.sisem.ui.map
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Menu
 import androidx.compose.material3.DrawerState
@@ -13,6 +16,8 @@ import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.skgtecnologia.sisem.ui.menu.MenuDrawer
 import com.skgtecnologia.sisem.ui.navigation.MainNavigationRoute
 import com.valkiria.uicomponents.components.map.MapComponent
@@ -50,12 +55,17 @@ fun MapScreen(
                     coroutineScope.launch {
                         drawerState.open()
                     }
-                }
+                },
+                modifier = Modifier.padding(12.dp)
             ) {
                 Icon(
                     imageVector = Icons.Outlined.Menu,
                     contentDescription = null,
-                    tint = MaterialTheme.colorScheme.primary
+                    modifier = Modifier
+                        .background(color = MaterialTheme.colorScheme.primary)
+                        .size(72.dp)
+                        .padding(5.dp),
+                    tint = Color.Black
                 )
             }
         }
