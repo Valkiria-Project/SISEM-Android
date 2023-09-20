29011c12e66e18bc57638db02aa12694f4df7f5c
Juan Vilada <jvillad1@gmail.com>
2023-09-20 09:52:11 -0500

    fix: add hardcoded onAction for DateTextFieldView (WIP)

--- uicomponents/src/main/java/com/valkiria/uicomponents/bricks/textfield/DateTextFieldView.kt
+++ uicomponents/src/main/java/com/valkiria/uicomponents/bricks/textfield/DateTextFieldView.kt
@@ -12,12 +12,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import com.valkiria.uicomponents.R
 import com.valkiria.uicomponents.model.mocks.getPreOpDriverAuxGuardianTextFieldUiModel
@@ -33,23 +35,31 @@ fun DateTextFieldView(
     validateFields: Boolean,
     onAction: (id: String, updatedValue: String, fieldValidated: Boolean) -> Unit
 ) {
-    var showDialog by remember { mutableStateOf(false) }
-
-    val selectedStartDate by remember {
-        val today = getLocalDateFromInstant(Instant.now())
-        mutableStateOf(today)
-    }
 
     val startLabel = buildString {
-        val dayOfMonth = selectedStartDate.dayOfMonth.toString().padStart(2, '0')
-        val month = selectedStartDate.monthValue.toString().padStart(2, '0')
-        val year = selectedStartDate.year.toString()
+        val today = getLocalDateFromInstant(Instant.now())
+        val dayOfMonth = today.dayOfMonth.toString().padStart(2, '0')
+        val month = today.monthValue.toString().padStart(2, '0')
+        val year = today.year.toString()
 
         append("$dayOfMonth/$month/$year ${getFormattedLocalTimeAsString()}")
     }
 
+    val date by rememberSaveable(stateSaver = TextFieldValue.Saver) {
+        mutableStateOf(TextFieldValue(startLabel))
+    }
+
+    // FIXME: Do this at the dialog level
+    onAction(
+        uiModel.identifier,
+        date.text,
+        true
+    )
+
+    var showDialog by remember { mutableStateOf(false) }
+
     OutlinedTextField(
-        value = startLabel,
+        value = date,
         onValueChange = {},
         readOnly = true,
         modifier = Modifier
