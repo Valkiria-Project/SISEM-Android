99bd3666377e152d4b580d29fe953606dd2dcc3b
RBarrios <rodolfo.9408@gmail.com>
2023-09-28 22:46:41 -0500

    fix: Make CI Happy

--- app/src/main/java/com/skgtecnologia/sisem/ui/humanbody/wounds/WoundsContent.kt
+++ app/src/main/java/com/skgtecnologia/sisem/ui/humanbody/wounds/WoundsContent.kt
@@ -24,6 +24,7 @@ import com.valkiria.uicomponents.model.ui.chip.ChipOptionsUiModel
 import com.valkiria.uicomponents.model.ui.chip.ChipSelectionItemUiModel
 import com.valkiria.uicomponents.model.ui.chip.ChipSelectionUiModel
 
+@Suppress("LongMethod")
 @Composable
 fun WoundsContent(
     onAction: (wounds: List<String>) -> Unit
