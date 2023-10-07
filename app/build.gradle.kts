27d367480cb44063d002a3ce5313d5a7e339780b
Juan Vilada <jvillad1@gmail.com>
2023-10-06 22:06:21 -0500

    chore: bump version to 0.3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         minSdk = 30
         targetSdk = 34
         versionCode = 6
-        versionName = "0.3.1"
+        versionName = "0.3.2"
         setProperty("archivesBaseName", "$applicationId-v$versionName($versionCode)")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
