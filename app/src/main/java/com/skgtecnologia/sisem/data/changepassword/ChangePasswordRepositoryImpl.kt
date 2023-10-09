205b811bc54d155d798a08c32015c0d3fda9b9b0
RBarrios <rodolfo.9408@gmail.com>
2023-10-09 00:28:12 -0500

    [Fix]: Auth navigation (#104)

--- app/src/main/java/com/skgtecnologia/sisem/data/changepassword/ChangePasswordRepositoryImpl.kt
+++ app/src/main/java/com/skgtecnologia/sisem/data/changepassword/ChangePasswordRepositoryImpl.kt
@@ -1,11 +1,14 @@
 package com.skgtecnologia.sisem.data.changepassword
 
+import com.skgtecnologia.sisem.data.auth.cache.AuthCacheDataSource
 import com.skgtecnologia.sisem.data.changepassword.remote.ChangePasswordRemoteDataSource
 import com.skgtecnologia.sisem.domain.changepassword.ChangePasswordRepository
 import com.skgtecnologia.sisem.domain.model.screen.ScreenModel
+import kotlinx.coroutines.flow.first
 import javax.inject.Inject
 
 class ChangePasswordRepositoryImpl @Inject constructor(
+    private val authCacheDataSource: AuthCacheDataSource,
     private val changePasswordRemoteDataSource: ChangePasswordRemoteDataSource
 ) : ChangePasswordRepository {
 
@@ -18,5 +21,9 @@ class ChangePasswordRepositoryImpl @Inject constructor(
     ): String = changePasswordRemoteDataSource.changePassword(
         oldPassword = oldPassword,
         newPassword = newPassword
-    ).getOrThrow()
+    ).onSuccess {
+        authCacheDataSource.observeAccessToken().first()?.let { accessToken ->
+            authCacheDataSource.storeAccessToken(accessToken.copy(isWarning = false))
+        }
+    }.getOrThrow()
 }
