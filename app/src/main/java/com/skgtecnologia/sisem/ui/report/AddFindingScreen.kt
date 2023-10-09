b09030d88acf4e3f64caf290b4c96ff02a9f0789
Juan Vilada <jvillad1@gmail.com>
2023-10-08 22:50:41 -0500

    chore: make CI happy?

--- app/src/main/java/com/skgtecnologia/sisem/ui/report/AddFindingScreen.kt
+++ app/src/main/java/com/skgtecnologia/sisem/ui/report/AddFindingScreen.kt
@@ -157,7 +157,9 @@ private fun handleFooterAction(
             AddFindingIdentifier.ADD_FINDING_CANCEL_BUTTON.name -> viewModel.cancelFinding()
             AddFindingIdentifier.ADD_FINDING_SAVE_BUTTON.name -> viewModel.saveFinding()
 
-            AddFindingIdentifier.ADD_FINDING_CANCEL_BANNER.name -> viewModel.consumeNavigationEvent()
+            AddFindingIdentifier.ADD_FINDING_CANCEL_BANNER.name ->
+                viewModel.consumeNavigationEvent()
+
             AddFindingIdentifier.ADD_FINDING_CONTINUE_BANNER.name -> viewModel.navigateBack()
         }
     }
