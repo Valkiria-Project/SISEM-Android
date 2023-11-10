49c900f763a2bcaa22c978edf5c965c8b8b3084e
Juan Vilada <jvillad1@gmail.com>
2023-11-10 00:00:23 -0500

    test: improve logic for SplashViewModelTest and add ExperimentalCoroutinesApi to Build_gradle opt-in

--- app/src/test/java/com/skgtecnologia/sisem/commons/MainDispatcherRule.kt
+++ app/src/test/java/com/skgtecnologia/sisem/commons/MainDispatcherRule.kt
@@ -1,7 +1,6 @@
 package com.skgtecnologia.sisem.commons
 
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.TestDispatcher
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.resetMain
@@ -9,9 +8,8 @@ import kotlinx.coroutines.test.setMain
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 
-@OptIn(ExperimentalCoroutinesApi::class)
 class MainDispatcherRule(
-    val testDispatcher: TestDispatcher = UnconfinedTestDispatcher(),
+    private val testDispatcher: TestDispatcher = UnconfinedTestDispatcher(),
 ) : TestWatcher() {
 
     override fun starting(description: Description) {
