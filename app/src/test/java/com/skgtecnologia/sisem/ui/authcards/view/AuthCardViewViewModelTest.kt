8d03cb5150c35ec34cf2c43fdcf3e44f2abb9662
Juan Vilada <jvillad1@gmail.com>
2023-11-10 22:53:53 -0500

    test: align some stuff with newer screens

--- app/src/test/java/com/skgtecnologia/sisem/ui/authcards/view/AuthCardViewViewModelTest.kt
+++ app/src/test/java/com/skgtecnologia/sisem/ui/authcards/view/AuthCardViewViewModelTest.kt
@@ -1,10 +1,12 @@
 package com.skgtecnologia.sisem.ui.authcards.view
 
+import com.skgtecnologia.sisem.commons.ANDROID_ID
 import com.skgtecnologia.sisem.commons.MainDispatcherRule
+import com.skgtecnologia.sisem.commons.SERVER_ERROR_TITLE
+import com.skgtecnologia.sisem.commons.emptyScreenModel
 import com.skgtecnologia.sisem.commons.resources.AndroidIdProvider
 import com.skgtecnologia.sisem.di.operation.OperationRole
 import com.skgtecnologia.sisem.domain.auth.usecases.LogoutCurrentUser
-import com.skgtecnologia.sisem.domain.model.screen.ScreenModel
 import com.skgtecnologia.sisem.domain.preoperational.model.PreOperationalViewIdentifier
 import com.skgtecnologia.sisem.domain.preoperational.usecases.GetAuthCardViewScreen
 import io.mockk.MockKAnnotations
@@ -16,9 +18,6 @@ import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-
-private const val ANDROID_ID = "123"
-private const val SERVER_ERROR_TITLE = "Error en servidor"
 
 class AuthCardViewViewModelTest {
 
@@ -34,8 +33,6 @@ class AuthCardViewViewModelTest {
     @MockK
     private lateinit var getAuthCardViewScreen: GetAuthCardViewScreen
 
-    private val screenModel = ScreenModel(body = emptyList())
-
     private lateinit var viewModel: AuthCardViewViewModel
 
     @Before
@@ -46,91 +43,110 @@ class AuthCardViewViewModelTest {
     }
 
     @Test
-    fun `when init view model getAuthCardViewScreen is success`() = runTest {
-        coEvery { getAuthCardViewScreen.invoke(any()) } returns Result.success(screenModel)
+    fun `when call init and getAuthCardViewScreen is success`() = runTest {
+        coEvery { getAuthCardViewScreen.invoke(any()) } returns Result.success(emptyScreenModel)
 
-        viewModel =
-            AuthCardViewViewModel(androidIdProvider, logoutCurrentUser, getAuthCardViewScreen)
+        viewModel = AuthCardViewViewModel(
+            androidIdProvider,
+            logoutCurrentUser,
+            getAuthCardViewScreen
+        )
 
-        Assert.assertEquals(screenModel, viewModel.uiState.screenModel)
+        Assert.assertEquals(emptyScreenModel, viewModel.uiState.screenModel)
     }
 
     @Test
-    fun `when init view model getAuthCardViewScreen fails`() = runTest {
+    fun `when call init and getAuthCardViewScreen fails`() = runTest {
         coEvery { getAuthCardViewScreen.invoke(any()) } returns Result.failure(
             IllegalStateException()
         )
 
-        viewModel =
-            AuthCardViewViewModel(androidIdProvider, logoutCurrentUser, getAuthCardViewScreen)
+        viewModel = AuthCardViewViewModel(
+            androidIdProvider,
+            logoutCurrentUser,
+            getAuthCardViewScreen
+        )
 
         Assert.assertEquals(SERVER_ERROR_TITLE, viewModel.uiState.errorModel?.title)
     }
 
     @Test
-    fun `when goBack is called navigationModel back is true`() = runTest {
-        coEvery { getAuthCardViewScreen.invoke(any()) } returns Result.success(screenModel)
+    fun `when call goBack uiState should have back true`() = runTest {
+        coEvery { getAuthCardViewScreen.invoke(any()) } returns Result.success(emptyScreenModel)
 
-        viewModel =
-            AuthCardViewViewModel(androidIdProvider, logoutCurrentUser, getAuthCardViewScreen)
-
+        viewModel = AuthCardViewViewModel(
+            androidIdProvider,
+            logoutCurrentUser,
+            getAuthCardViewScreen
+        )
         viewModel.goBack()
 
         Assert.assertEquals(true, viewModel.uiState.navigationModel?.back)
     }
 
     @Test
-    fun `when navigate is called with assistant identifier navigationModel role is correct`() = runTest {
-        coEvery { getAuthCardViewScreen.invoke(any()) } returns Result.success(screenModel)
+    fun `when call navigate with assistant navigationModel role is assistant`() =
+        runTest {
+            coEvery { getAuthCardViewScreen.invoke(any()) } returns Result.success(emptyScreenModel)
 
-        viewModel =
-            AuthCardViewViewModel(androidIdProvider, logoutCurrentUser, getAuthCardViewScreen)
+            viewModel = AuthCardViewViewModel(
+                androidIdProvider,
+                logoutCurrentUser,
+                getAuthCardViewScreen
+            )
+            viewModel.navigate(PreOperationalViewIdentifier.CREW_MEMBER_CARD_ASSISTANT.name)
 
-        viewModel.navigate(PreOperationalViewIdentifier.CREW_MEMBER_CARD_ASSISTANT.name)
-
-        Assert.assertEquals(
-            OperationRole.AUXILIARY_AND_OR_TAPH,
-            viewModel.uiState.navigationModel?.role
-        )
-    }
-
-    @Test
-    fun `when navigate is called with driver identifier navigationModel role is correct`() = runTest {
-        coEvery { getAuthCardViewScreen.invoke(any()) } returns Result.success(screenModel)
-
-        viewModel =
-            AuthCardViewViewModel(androidIdProvider, logoutCurrentUser, getAuthCardViewScreen)
-
-        viewModel.navigate(PreOperationalViewIdentifier.CREW_MEMBER_CARD_DRIVER.name)
-
-        Assert.assertEquals(
-            OperationRole.DRIVER,
-            viewModel.uiState.navigationModel?.role
-        )
-    }
+            Assert.assertEquals(
+                OperationRole.AUXILIARY_AND_OR_TAPH,
+                viewModel.uiState.navigationModel?.role
+            )
+        }
 
     @Test
-    fun `when navigate is called with doctor identifier navigationModel role is correct`() = runTest {
-        coEvery { getAuthCardViewScreen.invoke(any()) } returns Result.success(screenModel)
+    fun `when call navigate with driver navigationModel role is driver`() =
+        runTest {
+            coEvery { getAuthCardViewScreen.invoke(any()) } returns Result.success(emptyScreenModel)
 
-        viewModel =
-            AuthCardViewViewModel(androidIdProvider, logoutCurrentUser, getAuthCardViewScreen)
+            viewModel = AuthCardViewViewModel(
+                androidIdProvider,
+                logoutCurrentUser,
+                getAuthCardViewScreen
+            )
+            viewModel.navigate(PreOperationalViewIdentifier.CREW_MEMBER_CARD_DRIVER.name)
 
-        viewModel.navigate(PreOperationalViewIdentifier.CREW_MEMBER_CARD_DOCTOR.name)
-
-        Assert.assertEquals(
-            OperationRole.MEDIC_APH,
-            viewModel.uiState.navigationModel?.role
-        )
-    }
+            Assert.assertEquals(
+                OperationRole.DRIVER,
+                viewModel.uiState.navigationModel?.role
+            )
+        }
 
     @Test
-    fun `when navigate is called with unknown identifier failure and error model is not null`() = runTest {
-        coEvery { getAuthCardViewScreen.invoke(any()) } returns Result.success(screenModel)
+    fun `when call navigate with doctor navigationModel role is doctor`() =
+        runTest {
+            coEvery { getAuthCardViewScreen.invoke(any()) } returns Result.success(emptyScreenModel)
 
-        viewModel =
-            AuthCardViewViewModel(androidIdProvider, logoutCurrentUser, getAuthCardViewScreen)
+            viewModel = AuthCardViewViewModel(
+                androidIdProvider,
+                logoutCurrentUser,
+                getAuthCardViewScreen
+            )
+            viewModel.navigate(PreOperationalViewIdentifier.CREW_MEMBER_CARD_DOCTOR.name)
 
+            Assert.assertEquals(
+                OperationRole.MEDIC_APH,
+                viewModel.uiState.navigationModel?.role
+            )
+        }
+
+    @Test
+    fun `when call navigate with unknown should fail with error model`() = runTest {
+        coEvery { getAuthCardViewScreen.invoke(any()) } returns Result.success(emptyScreenModel)
+
+        viewModel = AuthCardViewViewModel(
+            androidIdProvider,
+            logoutCurrentUser,
+            getAuthCardViewScreen
+        )
         viewModel.navigate("other")
 
         Assert.assertNotNull(viewModel.uiState.errorModel)
@@ -138,13 +154,15 @@ class AuthCardViewViewModelTest {
     }
 
     @Test
-    fun `when onNavigationHandled is called clear uiState`() = runTest {
-        coEvery { getAuthCardViewScreen.invoke(any()) } returns Result.success(screenModel)
+    fun `when call onNavigationHandled uiState should have navigationModel clear`() = runTest {
+        coEvery { getAuthCardViewScreen.invoke(any()) } returns Result.success(emptyScreenModel)
 
-        viewModel =
-            AuthCardViewViewModel(androidIdProvider, logoutCurrentUser, getAuthCardViewScreen)
-
-        viewModel.onNavigationHandled()
+        viewModel = AuthCardViewViewModel(
+            androidIdProvider,
+            logoutCurrentUser,
+            getAuthCardViewScreen
+        )
+        viewModel.consumeNavigationEvent()
 
         Assert.assertEquals(null, viewModel.uiState.navigationModel)
         Assert.assertEquals(false, viewModel.uiState.isLoading)
