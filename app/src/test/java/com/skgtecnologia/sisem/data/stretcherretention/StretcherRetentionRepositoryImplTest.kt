39ea120c5920602ad33d635b6cf792211545ed0d
RBarrios <rodolfo.9408@gmail.com>
2023-11-13 23:12:07 -0500

    test: stretcher retentention data module

---
+++ app/src/test/java/com/skgtecnologia/sisem/data/stretcherretention/StretcherRetentionRepositoryImplTest.kt
@@ -0,0 +1,5 @@
+package com.skgtecnologia.sisem.data.stretcherretention
+
+import org.junit.jupiter.api.Assertions.*
+
+class StretcherRetentionRepositoryImplTest
---
+++ app/src/test/java/com/skgtecnologia/sisem/data/stretcherretention/remote/StretcherRetentionRemoteDataSourceTest.kt
@@ -0,0 +1,5 @@
+package com.skgtecnologia.sisem.data.stretcherretention.remote
+
+import org.junit.jupiter.api.Assertions.*
+
+class StretcherRetentionRemoteDataSourceTest
