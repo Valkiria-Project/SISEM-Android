c81ba6d8c9f9abc28446d7bf44ac297521df879e
RBarrios <rodolfo.9408@gmail.com>
2023-12-02 15:41:13 -0500

    [Fix]: Drop down visibility (#176)
    Co-authored-by: Juan Vilada <jvillad1@gmail.com>

--- app/src/main/java/com/skgtecnologia/sisem/ui/dropdown/DropDownComponent.kt
+++ app/src/main/java/com/skgtecnologia/sisem/ui/dropdown/DropDownComponent.kt
@@ -18,7 +18,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -51,9 +50,7 @@ fun DropDownComponent(
 ) {
     var showDialog by remember { mutableStateOf(false) }
 
-    var text by rememberSaveable(stateSaver = TextFieldValue.Saver) {
-        mutableStateOf(TextFieldValue(uiModel.selected))
-    }
+    var text by remember { mutableStateOf(TextFieldValue(uiModel.selected)) }
 
     val focusManager = LocalFocusManager.current
 
