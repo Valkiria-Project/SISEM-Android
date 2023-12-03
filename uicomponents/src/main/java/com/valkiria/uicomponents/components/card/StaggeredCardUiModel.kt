1e2df2b7207558e070d65a4f311528ad4db03927
RBarrios <rodolfo.9408@gmail.com>
2023-12-02 22:29:13 -0500

    [Feat]: Staggered cards component (#178)
    Co-authored-by: Juan Vilada <jvillad1@gmail.com>

---
+++ uicomponents/src/main/java/com/valkiria/uicomponents/components/card/StaggeredCardUiModel.kt
@@ -0,0 +1,8 @@
+package com.valkiria.uicomponents.components.card
+
+import com.valkiria.uicomponents.components.label.TextUiModel
+
+data class StaggeredCardUiModel(
+    val label: TextUiModel,
+    val value: TextUiModel
+)
