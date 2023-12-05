ae32faf4213737135ce7aed26cbf3908ede58432
Juan Vilada <jvillad1@gmail.com>
2023-12-05 07:39:38 -0500

    feat: add header for NotificationsRenderer

--- uicomponents/src/main/java/com/valkiria/uicomponents/bricks/map/MapboxMapView.kt
+++ uicomponents/src/main/java/com/valkiria/uicomponents/bricks/map/MapboxMapView.kt
@@ -7,7 +7,9 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
@@ -20,6 +22,7 @@ import androidx.compose.material3.DrawerState
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -30,8 +33,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.compose.ui.viewinterop.NoOpUpdate
 import androidx.compose.ui.window.Dialog
@@ -160,27 +168,8 @@ fun MapboxMapView(
                 }
             }
 
-            if (showNotificationsDialog) {
-                Dialog(
-                    onDismissRequest = { showNotificationsDialog = false },
-                    properties = DialogProperties(usePlatformDefaultWidth = false)
-                ) {
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .background(MaterialTheme.colorScheme.background.copy(alpha = 0.8f))
-                            .clickable(
-                                enabled = false,
-                                onClick = {}
-                            )
-                            .verticalScroll(rememberScrollState()),
-                        verticalArrangement = Arrangement.Top
-                    ) {
-                        notifications?.forEach { notificationUiModel ->
-                            NotificationRowView(notificationUiModel)
-                        }
-                    }
-                }
+            NotificationsRenderer(showNotificationsDialog, notifications) {
+                showNotificationsDialog = false
             }
         }
     }
@@ -299,4 +288,61 @@ private fun MapboxNavigationAndroidView(
         },
         modifier = modifier
     )
+}
+
+@Composable
+private fun NotificationsRenderer(
+    showNotificationsDialog: Boolean,
+    notifications: List<NotificationUiModel>?,
+    onAction: () -> Unit
+) {
+    if (showNotificationsDialog) {
+        Dialog(
+            onDismissRequest = { onAction() },
+            properties = DialogProperties(usePlatformDefaultWidth = false)
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(MaterialTheme.colorScheme.background.copy(alpha = 0.8f))
+                    .clickable(
+                        enabled = false,
+                        onClick = {}
+                    )
+                    .verticalScroll(rememberScrollState()),
+                verticalArrangement = Arrangement.Top
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        imageVector = ImageVector.vectorResource(id = R.drawable.ic_back),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .clickable { onAction() }
+                            .padding(20.dp)
+                            .size(32.dp),
+                        tint = Color.White
+                    )
+
+                    Text(
+                        text = stringResource(R.string.notifications_title),
+                        modifier = Modifier
+                            .weight(1f)
+                            .padding(20.dp),
+                        color = Color.White,
+                        style = MaterialTheme.typography.displayLarge.copy(
+                            fontSize = 24.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                    )
+                }
+
+                notifications?.forEach { notificationUiModel ->
+                    NotificationRowView(notificationUiModel)
+                }
+            }
+        }
+    }
 }
