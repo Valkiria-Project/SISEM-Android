143290c958140b2769717ea8c207036b854d364f
Juan Vilada <jvillad1@gmail.com>
2023-12-12 05:35:06 -0500

    fix: SMA-337

--- uicomponents/src/main/java/com/valkiria/uicomponents/bricks/button/ButtonView.kt
+++ uicomponents/src/main/java/com/valkiria/uicomponents/bricks/button/ButtonView.kt
@@ -5,10 +5,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import com.valkiria.uicomponents.components.button.ButtonSize
+import com.valkiria.uicomponents.components.button.ButtonUiModel
 import com.valkiria.uicomponents.components.button.mapToColors
 import com.valkiria.uicomponents.components.button.mapToTextColor
 import com.valkiria.uicomponents.components.label.toTextStyle
-import com.valkiria.uicomponents.components.button.ButtonUiModel
 
 @Suppress("UnusedPrivateMember")
 @Composable
@@ -28,7 +28,7 @@ fun ButtonView(
         Text(
             text = uiModel.label,
             color = uiModel.style.mapToTextColor(uiModel.overrideColor),
-            style = uiModel.textStyle.toTextStyle()
+            style = uiModel.composeTextStyle ?: uiModel.textStyle.toTextStyle()
         )
     }
 }
