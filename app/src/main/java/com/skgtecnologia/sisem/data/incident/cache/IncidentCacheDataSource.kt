5982e3c9171f74fdc1bc65dc664244e6298093bb
Juan Vilada <jvillad1@gmail.com>
2023-12-12 06:34:08 -0500

    feat: SMA-250

--- app/src/main/java/com/skgtecnologia/sisem/data/incident/cache/IncidentCacheDataSource.kt
+++ app/src/main/java/com/skgtecnologia/sisem/data/incident/cache/IncidentCacheDataSource.kt
@@ -4,6 +4,8 @@ import androidx.annotation.CheckResult
 import com.skgtecnologia.sisem.data.incident.cache.dao.IncidentDao
 import com.skgtecnologia.sisem.data.incident.cache.model.mapToCache
 import com.skgtecnologia.sisem.data.incident.cache.model.mapToUi
+import com.valkiria.uicomponents.bricks.notification.model.TransmiNotification
+import com.valkiria.uicomponents.bricks.notification.model.getTransmiNotificationRawDataByType
 import com.valkiria.uicomponents.components.incident.model.IncidentUiModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
@@ -18,6 +20,15 @@ class IncidentCacheDataSource @Inject constructor(
 
     suspend fun storeIncident(incidentUiModel: IncidentUiModel) =
         incidentDao.insertIncident(incidentUiModel.mapToCache())
+
+    suspend fun updateTransmiStatus(
+        incidentId: Long,
+        transmiNotifications: List<TransmiNotification>
+    ) {
+        incidentDao.updateTransmiStatus(
+            incidentId,
+            transmiNotifications.map { getTransmiNotificationRawDataByType(it) })
+    }
 
     @CheckResult
     fun observeActiveIncident(): Flow<IncidentUiModel?> = incidentDao.observeActiveIncident()
