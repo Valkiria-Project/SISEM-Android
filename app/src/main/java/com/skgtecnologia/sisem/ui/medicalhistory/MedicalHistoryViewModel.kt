db1760425a7b398ffc12ba0f879db606b331ea08
Juan Vilada <jvillad1@gmail.com>
2024-02-19 23:42:45 -0500

    fix: Lint

--- app/src/main/java/com/skgtecnologia/sisem/ui/medicalhistory/MedicalHistoryViewModel.kt
+++ app/src/main/java/com/skgtecnologia/sisem/ui/medicalhistory/MedicalHistoryViewModel.kt
@@ -290,7 +290,7 @@ class MedicalHistoryViewModel @Inject constructor(
                 chipOption.remove(chipOptionAction.chipOptionUiModel.id)
 
             chipOption != null &&
-                    chipOption.contains(chipOptionAction.chipOptionUiModel.id).not() -> {
+                chipOption.contains(chipOptionAction.chipOptionUiModel.id).not() -> {
                 chipOption.add(chipOptionAction.chipOptionUiModel.id)
             }
 
@@ -490,7 +490,7 @@ class MedicalHistoryViewModel @Inject constructor(
                                 options = it.options.map { imageButtonUiModel ->
                                     imageButtonUiModel.copy(
                                         selected = imageButtonUiModel.identifier ==
-                                                imageButtonAction.itemIdentifier
+                                            imageButtonAction.itemIdentifier
                                     )
                                 }
                             )
@@ -636,9 +636,9 @@ class MedicalHistoryViewModel @Inject constructor(
 
     private fun SnapshotStateMap<String, InputUiModel>.getPatientName(): String =
         this[FIRST_NAME_KEY]?.updatedValue.orEmpty() + " " +
-                this[SECOND_NAME_KEY]?.updatedValue.orEmpty() + " " +
-                this[LASTNAME_KEY]?.updatedValue.orEmpty() + " " +
-                this[SECOND_LASTNAME_KEY]?.updatedValue.orEmpty()
+            this[SECOND_NAME_KEY]?.updatedValue.orEmpty() + " " +
+            this[LASTNAME_KEY]?.updatedValue.orEmpty() + " " +
+            this[SECOND_LASTNAME_KEY]?.updatedValue.orEmpty()
 
     private fun SnapshotStateMap<String, InputUiModel>.getPatientDocument(): String =
         this[DOCUMENT_KEY]?.updatedValue.orEmpty()
