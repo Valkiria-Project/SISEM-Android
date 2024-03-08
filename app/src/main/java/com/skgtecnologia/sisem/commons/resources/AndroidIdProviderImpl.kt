c0ec0b626d9fadd9267d1f28b911db86da3ded6e
RBarrios <rodolfo.9408@gmail.com>
2024-03-08 13:32:22 -0500

    [Feat]: Storage android id (#197)

--- app/src/main/java/com/skgtecnologia/sisem/commons/resources/AndroidIdProviderImpl.kt
+++ app/src/main/java/com/skgtecnologia/sisem/commons/resources/AndroidIdProviderImpl.kt
@@ -1,14 +1,37 @@
 package com.skgtecnologia.sisem.commons.resources
 
 import android.content.Context
+import android.provider.Settings
 import dagger.hilt.android.qualifiers.ApplicationContext
+import java.io.File
 import javax.inject.Inject
+
+private const val FILE_NAME = "android_id"
 
 @Suppress("UnusedPrivateMember")
 class AndroidIdProviderImpl @Inject constructor(
     @ApplicationContext private val context: Context
 ) : AndroidIdProvider {
 
-    override fun getAndroidId(): String = "12312JHJKG22" // FIXME: Hardcoded data
-    // Settings.Secure.getString(context.contentResolver, Settings.Secure.ANDROID_ID).orEmpty()
+    override fun getAndroidId(): String {
+        var androidId: String
+
+        File(context.filesDir, FILE_NAME).createNewFile()
+
+        context.openFileInput(FILE_NAME).use { input ->
+            androidId = input.readBytes().toString(Charsets.UTF_8)
+        }
+
+        if (androidId.isEmpty()) {
+            androidId = Settings
+                .Secure
+                .getString(context.contentResolver, Settings.Secure.ANDROID_ID)
+                .orEmpty()
+            context.openFileOutput(FILE_NAME, Context.MODE_PRIVATE).use { output ->
+                output.write(androidId.toByteArray())
+            }
+        }
+
+        return androidId
+    }
 }
