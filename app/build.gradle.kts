cac7a1f58e26ebcebcc7c30c3c77d9fe9955b5cf
Juan Vilada <jvillad1@gmail.com>
2024-03-13 04:24:31 -0500

    chore: bump version to 1.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.skgtecnologia.sisem"
         minSdk = 30
         targetSdk = 34
-        versionCode = 33
-        versionName = "1.4.0"
+        versionCode = 34
+        versionName = "1.5.0"
         setProperty("archivesBaseName", "$applicationId-v$versionName($versionCode)")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
