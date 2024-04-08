9b8434e9f9ee33fe5aead732259eb2e79c7f3a46
Juan Camilo Villada <jvillad1@gmail.com>
2024-04-08 12:07:05 -0500

    [chore]: bump app version to 1.5.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.skgtecnologia.sisem"
         minSdk = 30
         targetSdk = 34
-        versionCode = 38
-        versionName = "1.5.4"
+        versionCode = 39
+        versionName = "1.5.5"
         setProperty("archivesBaseName", "$applicationId-v$versionName($versionCode)")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
