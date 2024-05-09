c7a8ec12836869dcecb44de1ab7c945c8dcd8dd2
Juan Camilo Villada <jvillad1@gmail.com>
2024-05-09 01:13:50 -0400

    [chore]: Bump version to 1.8.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.skgtecnologia.sisem"
         minSdk = 30
         targetSdk = 34
-        versionCode = 49
-        versionName = "1.8.6"
+        versionCode = 50
+        versionName = "1.8.7"
         setProperty("archivesBaseName", "$applicationId-v$versionName($versionCode)")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
