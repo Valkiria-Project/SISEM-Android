c2bb0a10a2ada1c3e1c33f9790f2737747c222fa
Juan Vilada <jvillad1@gmail.com>
2024-05-27 21:49:38 -0500

    [fix]: SMA-468

--- app/src/main/java/com/skgtecnologia/sisem/data/location/LocationRepositoryImpl.kt
+++ app/src/main/java/com/skgtecnologia/sisem/data/location/LocationRepositoryImpl.kt
@@ -3,9 +3,11 @@ package com.skgtecnologia.sisem.data.location
 import com.skgtecnologia.sisem.commons.resources.AndroidIdProvider
 import com.skgtecnologia.sisem.data.incident.cache.IncidentCacheDataSource
 import com.skgtecnologia.sisem.data.location.remote.LocationRemoteDataSource
+import com.skgtecnologia.sisem.data.notification.cache.NotificationCacheDataSource
 import com.skgtecnologia.sisem.data.operation.cache.OperationCacheDataSource
 import com.skgtecnologia.sisem.domain.location.LocationRepository
 import com.valkiria.uicomponents.utlis.TimeUtils
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.firstOrNull
 import java.time.Instant
 import javax.inject.Inject
@@ -14,13 +16,15 @@ private const val MOBILE_APP_ORIGIN = 2
 
 class LocationRepositoryImpl @Inject constructor(
     private val androidIdProvider: AndroidIdProvider,
-    private val incidentCacheDataSource: IncidentCacheDataSource,
     private val locationRemoteDataSource: LocationRemoteDataSource,
+    private val notificationCacheDataSource: NotificationCacheDataSource,
     private val operationCacheDataSource: OperationCacheDataSource
 ) : LocationRepository {
 
     override suspend fun updateLocation(latitude: Double, longitude: Double) {
-        val incident = incidentCacheDataSource.observeActiveIncident().firstOrNull()
+        val currentIncidentNotification = notificationCacheDataSource
+            .getActiveIncidentNotification()
+            ?.first()
 
         locationRemoteDataSource.sendUpdatedLocation(
             idVehicle = operationCacheDataSource
@@ -32,7 +36,7 @@ class LocationRepositoryImpl @Inject constructor(
             latitude = latitude,
             longitude = longitude,
             originAt = TimeUtils.getLocalDateTime(Instant.now()).toString(),
-            idIncident = incident?.incident?.id?.toString()
+            idIncident = currentIncidentNotification?.incidentNumber
         )
     }
 }
