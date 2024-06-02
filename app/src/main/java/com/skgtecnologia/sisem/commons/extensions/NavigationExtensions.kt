a18639fa63ae59dfc2aa30b454870075012a6ac7
jvillad1 <jvillad1@gmail.com>
2024-06-02 13:07:04 -0500

    [feat]: Improve navigation popBackStack()

--- app/src/main/java/com/skgtecnologia/sisem/commons/extensions/NavigationExtensions.kt
+++ app/src/main/java/com/skgtecnologia/sisem/commons/extensions/NavigationExtensions.kt
@@ -6,8 +6,10 @@ import androidx.navigation.NavHostController
 val NavHostController.canGoBack: Boolean
     get() = this.currentBackStackEntry?.lifecycle?.currentState == Lifecycle.State.RESUMED
 
-fun NavHostController.navigateBack() {
-    if (canGoBack) {
+fun NavHostController.navigateBack(): Boolean {
+    return if (canGoBack) {
         popBackStack()
+    } else {
+        false
     }
 }
