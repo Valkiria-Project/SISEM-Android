cfd0df6969358593a51c0e39160843e7db3ab7c5
jvillad1 <jvillad1@gmail.com>
2024-06-04 01:04:19 -0500

    [fix]: SMA-531

--- app/src/main/java/com/skgtecnologia/sisem/ui/authcards/view/AuthCardViewScreen.kt
+++ app/src/main/java/com/skgtecnologia/sisem/ui/authcards/view/AuthCardViewScreen.kt
@@ -1,23 +1,29 @@
 package com.skgtecnologia.sisem.ui.authcards.view
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.skgtecnologia.sisem.commons.communication.NotificationEventHandler
+import com.skgtecnologia.sisem.ui.authcards.create.report.FindingsContent
+import com.skgtecnologia.sisem.ui.authcards.create.report.ReportDetailContent
+import com.skgtecnologia.sisem.ui.navigation.AuthNavigationRoute
 import com.skgtecnologia.sisem.ui.sections.BodySection
 import com.skgtecnologia.sisem.ui.sections.HeaderSection
 import com.valkiria.uicomponents.action.GenericUiAction
 import com.valkiria.uicomponents.action.HeaderUiAction
 import com.valkiria.uicomponents.action.UiAction
 import com.valkiria.uicomponents.bricks.banner.OnBannerHandler
+import com.valkiria.uicomponents.bricks.bottomsheet.BottomSheetView
 import com.valkiria.uicomponents.bricks.loader.OnLoadingHandler
 import com.valkiria.uicomponents.bricks.notification.OnNotificationHandler
 import com.valkiria.uicomponents.bricks.notification.model.NotificationData
@@ -31,6 +37,9 @@ fun AuthCardViewScreen(
 ) {
     val viewModel = hiltViewModel<AuthCardViewViewModel>()
     val uiState = viewModel.uiState
+
+    val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
+    val scope = rememberCoroutineScope()
 
     var notificationData by remember { mutableStateOf<NotificationData?>(null) }
     NotificationEventHandler.subscribeNotificationEvent {
@@ -80,6 +89,32 @@ fun AuthCardViewScreen(
         }
     }
 
+    uiState.reportDetail?.let {
+        scope.launch { sheetState.show() }
+
+        BottomSheetView(
+            content = { ReportDetailContent(model = uiState.reportDetail) },
+            sheetState = sheetState,
+            scope = scope
+        ) {
+            viewModel.consumeReportBottomSheetEvent()
+        }
+    }
+
+    uiState.chipSection?.let {
+        scope.launch { sheetState.show() }
+
+        BottomSheetView(
+            content = {
+                FindingsContent(chipSection = uiState.chipSection)
+            },
+            sheetState = sheetState,
+            scope = scope
+        ) {
+            viewModel.consumeFindingsBottomSheetEvent()
+        }
+    }
+
     OnNotificationHandler(notificationData) {
         notificationData = null
         if (it.isDismiss.not()) {
@@ -97,7 +132,19 @@ fun AuthCardViewScreen(
 
 private fun handleAction(uiAction: UiAction, viewModel: AuthCardViewViewModel) {
     when (uiAction) {
-        is GenericUiAction.InfoCardAction -> viewModel.navigate(uiAction.identifier)
+        is GenericUiAction.InfoCardAction -> {
+            if (uiAction.isClickCard) {
+                viewModel.navigate(uiAction.identifier)
+            }
+
+            uiAction.reportDetail?.let {
+                viewModel.showReportBottomSheet(it)
+            }
+
+            uiAction.chipSection?.let {
+                viewModel.showFindingsBottomSheet(it)
+            }
+        }
 
         else -> Timber.d("no-op")
     }
