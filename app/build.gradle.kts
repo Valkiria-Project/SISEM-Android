c826b32f8a175aea59136168617c2cb239ebdc2b
jvillad1 <jvillad1@gmail.com>
2024-07-25 02:17:31 -0500

    [chore]: Bump version to 1.9.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.skgtecnologia.sisem"
         minSdk = 30
         targetSdk = 34
-        versionCode = 60
-        versionName = "1.9.5"
+        versionCode = 61
+        versionName = "1.9.6"
         setProperty("archivesBaseName", "$applicationId-v$versionName($versionCode)")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
