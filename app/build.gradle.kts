767c39d3bf5bf6784aaa495c877d3896379221a7
jvillad1 <jvillad1@gmail.com>
2024-08-03 22:43:36 -0500

    [chore]: Bump version to 1.9.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.skgtecnologia.sisem"
         minSdk = 30
         targetSdk = 34
-        versionCode = 63
-        versionName = "1.9.8"
+        versionCode = 64
+        versionName = "1.9.9"
         setProperty("archivesBaseName", "$applicationId-v$versionName($versionCode)")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
