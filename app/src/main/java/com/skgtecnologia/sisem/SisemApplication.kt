228b676cea3400bb7c01ec955a2c224033bef12f
Juan Vilada <jvillad1@gmail.com>
2024-09-10 03:41:28 -0500

    [chore][mapBox-refactor]: Add callouts and clean the map look and feel

--- app/src/main/java/com/skgtecnologia/sisem/SisemApplication.kt
+++ app/src/main/java/com/skgtecnologia/sisem/SisemApplication.kt
@@ -4,11 +4,6 @@ import android.app.Application
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.FirebaseApp
 import com.google.firebase.messaging.FirebaseMessaging
-import com.mapbox.navigation.base.TimeFormat.TWENTY_FOUR_HOURS
-import com.mapbox.navigation.base.formatter.DistanceFormatterOptions
-import com.mapbox.navigation.base.formatter.UnitType
-import com.mapbox.navigation.base.options.NavigationOptions
-import com.mapbox.navigation.core.lifecycle.MapboxNavigationApp
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
@@ -36,18 +31,5 @@ class SisemApplication : Application() {
                 Timber.d("FCM registration token: ${task.result}")
             }
         )
-
-        if (!MapboxNavigationApp.isSetup()) {
-            MapboxNavigationApp.setup {
-                NavigationOptions.Builder(this)
-                    .distanceFormatterOptions(
-                        DistanceFormatterOptions.Builder(this)
-                            .unitType(UnitType.METRIC)
-                            .build()
-                    )
-                    .timeFormatType(TWENTY_FOUR_HOURS)
-                    .build()
-            }
-        }
     }
 }
--- uicomponents/src/main/java/com/valkiria/uicomponents/bricks/map/MapFragment.kt
+++ uicomponents/src/main/java/com/valkiria/uicomponents/bricks/map/MapFragment.kt
@@ -6,12 +6,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.mapbox.api.directions.v5.models.Bearing
 import com.mapbox.api.directions.v5.models.DirectionsRoute
 import com.mapbox.api.directions.v5.models.RouteOptions
-import com.mapbox.bindgen.Expected
 import com.mapbox.common.location.Location
 import com.mapbox.geojson.Point
 import com.mapbox.maps.EdgeInsets
@@ -21,17 +19,14 @@ import com.mapbox.maps.plugin.animation.camera
 import com.mapbox.maps.plugin.gestures.gestures
 import com.mapbox.maps.plugin.locationcomponent.location
 import com.mapbox.navigation.base.ExperimentalPreviewMapboxNavigationAPI
-import com.mapbox.navigation.base.TimeFormat
 import com.mapbox.navigation.base.extensions.applyDefaultNavigationOptions
 import com.mapbox.navigation.base.extensions.applyLanguageAndVoiceUnitOptions
-import com.mapbox.navigation.base.formatter.DistanceFormatterOptions
 import com.mapbox.navigation.base.options.NavigationOptions
 import com.mapbox.navigation.base.route.NavigationRoute
 import com.mapbox.navigation.base.route.NavigationRouterCallback
 import com.mapbox.navigation.base.route.RouterFailure
 import com.mapbox.navigation.core.MapboxNavigation
 import com.mapbox.navigation.core.directions.session.RoutesObserver
-import com.mapbox.navigation.core.formatter.MapboxDistanceFormatter
 import com.mapbox.navigation.core.lifecycle.MapboxNavigationApp
 import com.mapbox.navigation.core.lifecycle.MapboxNavigationObserver
 import com.mapbox.navigation.core.lifecycle.requireMapboxNavigation
@@ -40,15 +35,6 @@ import com.mapbox.navigation.core.replay.route.ReplayRouteMapper
 import com.mapbox.navigation.core.trip.session.LocationMatcherResult
 import com.mapbox.navigation.core.trip.session.LocationObserver
 import com.mapbox.navigation.core.trip.session.RouteProgressObserver
-import com.mapbox.navigation.core.trip.session.VoiceInstructionsObserver
-import com.mapbox.navigation.tripdata.maneuver.api.MapboxManeuverApi
-import com.mapbox.navigation.tripdata.progress.api.MapboxTripProgressApi
-import com.mapbox.navigation.tripdata.progress.model.DistanceRemainingFormatter
-import com.mapbox.navigation.tripdata.progress.model.EstimatedTimeToArrivalFormatter
-import com.mapbox.navigation.tripdata.progress.model.PercentDistanceTraveledFormatter
-import com.mapbox.navigation.tripdata.progress.model.TimeRemainingFormatter
-import com.mapbox.navigation.tripdata.progress.model.TripProgressUpdateFormatter
-import com.mapbox.navigation.ui.base.util.MapboxNavigationConsumer
 import com.mapbox.navigation.ui.maps.NavigationStyles
 import com.mapbox.navigation.ui.maps.camera.NavigationCamera
 import com.mapbox.navigation.ui.maps.camera.data.MapboxNavigationViewportDataSource
@@ -63,20 +49,18 @@ import com.mapbox.navigation.ui.maps.location.NavigationLocationProvider
 import com.mapbox.navigation.ui.maps.route.arrow.api.MapboxRouteArrowApi
 import com.mapbox.navigation.ui.maps.route.arrow.api.MapboxRouteArrowView
 import com.mapbox.navigation.ui.maps.route.arrow.model.RouteArrowOptions
+import com.mapbox.navigation.ui.maps.route.callout.api.MapboxRouteCalloutApi
+import com.mapbox.navigation.ui.maps.route.callout.api.MapboxRouteCalloutView
+import com.mapbox.navigation.ui.maps.route.callout.model.MapboxRouteCalloutApiOptions
+import com.mapbox.navigation.ui.maps.route.callout.model.MapboxRouteCalloutViewOptions
+import com.mapbox.navigation.ui.maps.route.callout.model.RouteCalloutType
 import com.mapbox.navigation.ui.maps.route.line.api.MapboxRouteLineApi
 import com.mapbox.navigation.ui.maps.route.line.api.MapboxRouteLineView
 import com.mapbox.navigation.ui.maps.route.line.model.MapboxRouteLineApiOptions
 import com.mapbox.navigation.ui.maps.route.line.model.MapboxRouteLineViewOptions
-import com.mapbox.navigation.voice.api.MapboxSpeechApi
-import com.mapbox.navigation.voice.api.MapboxVoiceInstructionsPlayer
-import com.mapbox.navigation.voice.model.SpeechAnnouncement
-import com.mapbox.navigation.voice.model.SpeechError
-import com.mapbox.navigation.voice.model.SpeechValue
-import com.mapbox.navigation.voice.model.SpeechVolume
 import com.valkiria.uicomponents.R
 import com.valkiria.uicomponents.databinding.FragmentMapBinding
 import java.util.Date
-import java.util.Locale
 
 private const val BUTTON_ANIMATION_DURATION = 1500L
 private const val PADDING_TOP_SMALL = 140.0
@@ -87,6 +71,7 @@ private const val PADDING_BOTTOM_LARGE = 150.0
 private const val BEARING_DEGREES = 45.0
 
 @Suppress("TooManyFunctions")
+@OptIn(ExperimentalPreviewMapboxNavigationAPI::class)
 class MapFragment : Fragment(R.layout.fragment_map) {
 
     private lateinit var binding: FragmentMapBinding
@@ -94,16 +79,34 @@ class MapFragment : Fragment(R.layout.fragment_map) {
     private var lastLocation: Location? = null
     private var destinationPoint: Point? = null
 
-    private lateinit var maneuverApi: MapboxManeuverApi
     private lateinit var navigationCamera: NavigationCamera
     private lateinit var replayProgressObserver: ReplayProgressObserver
     private lateinit var routeArrowView: MapboxRouteArrowView
     private lateinit var routeLineApi: MapboxRouteLineApi
     private lateinit var routeLineView: MapboxRouteLineView
-    private lateinit var speechApi: MapboxSpeechApi
-    private lateinit var tripProgressApi: MapboxTripProgressApi
     private lateinit var viewportDataSource: MapboxNavigationViewportDataSource
-    private lateinit var voiceInstructionsPlayer: MapboxVoiceInstructionsPlayer
+
+    private val routeCalloutApiOptions: MapboxRouteCalloutApiOptions by lazy {
+        MapboxRouteCalloutApiOptions.Builder()
+            .routeCalloutType(RouteCalloutType.RouteDurations)
+            .build()
+    }
+
+    private val routeCalloutViewOptions: MapboxRouteCalloutViewOptions by lazy {
+        MapboxRouteCalloutViewOptions.Builder()
+            .selectedBackgroundColor(R.color.sisem_route_callout_background)
+            .selectedTextColor(android.R.color.white)
+            .backgroundColor(android.R.color.darker_gray)
+            .build()
+    }
+
+    private val routeCalloutApi by lazy {
+        MapboxRouteCalloutApi(routeCalloutApiOptions)
+    }
+
+    private val routeCalloutView by lazy {
+        MapboxRouteCalloutView(binding.mapView, routeCalloutViewOptions)
+    }
 
     private val navigationLocationProvider = NavigationLocationProvider()
     private val replayRouteMapper = ReplayRouteMapper()
@@ -117,7 +120,6 @@ class MapFragment : Fragment(R.layout.fragment_map) {
                 mapboxNavigation.registerRoutesObserver(routesObserver)
                 mapboxNavigation.registerLocationObserver(locationObserver)
                 mapboxNavigation.registerRouteProgressObserver(routeProgressObserver)
-                mapboxNavigation.registerVoiceInstructionsObserver(voiceInstructionsObserver)
 
                 replayProgressObserver = ReplayProgressObserver(mapboxNavigation.mapboxReplayer)
                 mapboxNavigation.registerRouteProgressObserver(replayProgressObserver)
@@ -134,25 +136,34 @@ class MapFragment : Fragment(R.layout.fragment_map) {
                 mapboxNavigation.unregisterLocationObserver(locationObserver)
                 mapboxNavigation.unregisterRouteProgressObserver(routeProgressObserver)
                 mapboxNavigation.unregisterRouteProgressObserver(replayProgressObserver)
-                mapboxNavigation.unregisterVoiceInstructionsObserver(voiceInstructionsObserver)
                 mapboxNavigation.mapboxReplayer.finish()
             }
         }
     )
 
-    private val routesObserver = RoutesObserver { routeUpdateResult ->
-        if (routeUpdateResult.navigationRoutes.isNotEmpty()) {
+    private val routesObserver = RoutesObserver { result ->
+        if (result.navigationRoutes.isNotEmpty()) {
             // generate route geometries asynchronously and render them
             routeLineApi.setNavigationRoutes(
-                routeUpdateResult.navigationRoutes
+                result.navigationRoutes
             ) { value ->
                 binding.mapView.mapboxMap.style?.apply {
                     routeLineView.renderRouteDrawData(this, value)
                 }
             }
 
+            val alternativesMetadata = mapboxNavigation.getAlternativeMetadataFor(
+                result.navigationRoutes
+            )
+            routeCalloutApi.setNavigationRoutes(
+                result.navigationRoutes,
+                alternativesMetadata
+            ).also { value ->
+                routeCalloutView.renderCallouts(value)
+            }
+
             // update the camera position to account for the new route
-            viewportDataSource.onRouteChanged(routeUpdateResult.navigationRoutes.first())
+            viewportDataSource.onRouteChanged(result.navigationRoutes.first())
             viewportDataSource.evaluate()
         } else {
             // remove the route line and route arrow from the map
@@ -171,6 +182,8 @@ class MapFragment : Fragment(R.layout.fragment_map) {
             viewportDataSource.clearRouteData()
             viewportDataSource.evaluate()
         }
+
+
     }
     private val locationObserver = object : LocationObserver {
         var firstLocationUpdateReceived = false
@@ -215,55 +228,7 @@ class MapFragment : Fragment(R.layout.fragment_map) {
             val maneuverArrowResult = routeArrowApi.addUpcomingManeuverArrow(routeProgress)
             routeArrowView.renderManeuverUpdate(style, maneuverArrowResult)
         }
-
-        // update top banner with maneuver instructions
-        val maneuvers = maneuverApi.getManeuvers(routeProgress)
-        maneuvers.fold(
-            { error ->
-                Toast.makeText(
-                    requireActivity(),
-                    error.errorMessage,
-                    Toast.LENGTH_SHORT
-                ).show()
-            },
-            {
-                binding.maneuverView.visibility = View.VISIBLE
-                binding.maneuverView.renderManeuvers(maneuvers)
-            }
-        )
-
-        // update bottom trip progress summary
-        binding.tripProgressView.render(
-            tripProgressApi.getTripProgress(routeProgress)
-        )
     }
-    private val voiceInstructionsObserver = VoiceInstructionsObserver { voiceInstructions ->
-        speechApi.generate(voiceInstructions, speechCallback)
-    }
-    private val speechCallback =
-        MapboxNavigationConsumer<Expected<SpeechError, SpeechValue>> { expected ->
-            expected.fold(
-                { error ->
-                    // play the instruction via fallback text-to-speech engine
-                    voiceInstructionsPlayer.play(
-                        error.fallback,
-                        voiceInstructionsPlayerCallback
-                    )
-                },
-                { value ->
-                    // play the sound file from the external generator
-                    voiceInstructionsPlayer.play(
-                        value.announcement,
-                        voiceInstructionsPlayerCallback
-                    )
-                }
-            )
-        }
-    private val voiceInstructionsPlayerCallback =
-        MapboxNavigationConsumer<SpeechAnnouncement> { value ->
-            // remove already consumed file to free-up space
-            speechApi.clean(value)
-        }
     private val pixelDensity = Resources.getSystem().displayMetrics.density
     private val overviewPadding: EdgeInsets by lazy {
         EdgeInsets(
@@ -281,18 +246,6 @@ class MapFragment : Fragment(R.layout.fragment_map) {
             PADDING_HORIZONTAL * pixelDensity
         )
     }
-
-    private var isVoiceInstructionsMuted = false
-        set(value) {
-            field = value
-            if (value) {
-                binding.soundButton.muteAndExtend(BUTTON_ANIMATION_DURATION)
-                voiceInstructionsPlayer.volume(SpeechVolume(0f))
-            } else {
-                binding.soundButton.unmuteAndExtend(BUTTON_ANIMATION_DURATION)
-                voiceInstructionsPlayer.volume(SpeechVolume(1f))
-            }
-        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -340,42 +293,6 @@ class MapFragment : Fragment(R.layout.fragment_map) {
         viewportDataSource.overviewPadding = overviewPadding
         viewportDataSource.followingPadding = followingPadding
 
-        // make sure to use the same DistanceFormatterOptions across different features
-        val distanceFormatterOptions = DistanceFormatterOptions.Builder(requireActivity()).build()
-
-        // initialize maneuver api that feeds the data to the top banner maneuver view
-        maneuverApi = MapboxManeuverApi(
-            MapboxDistanceFormatter(distanceFormatterOptions)
-        )
-
-        // initialize bottom progress view
-        tripProgressApi = MapboxTripProgressApi(
-            TripProgressUpdateFormatter.Builder(requireActivity())
-                .distanceRemainingFormatter(
-                    DistanceRemainingFormatter(distanceFormatterOptions)
-                )
-                .timeRemainingFormatter(
-                    TimeRemainingFormatter(requireActivity())
-                )
-                .percentRouteTraveledFormatter(
-                    PercentDistanceTraveledFormatter()
-                )
-                .estimatedTimeToArrivalFormatter(
-                    EstimatedTimeToArrivalFormatter(requireActivity(), TimeFormat.NONE_SPECIFIED)
-                )
-                .build()
-        )
-
-        // initialize voice instructions api and the voice instruction player
-        speechApi = MapboxSpeechApi(
-            requireActivity(),
-            Locale.US.language
-        )
-        voiceInstructionsPlayer = MapboxVoiceInstructionsPlayer(
-            requireActivity(),
-            Locale.US.language
-        )
-
         // initialize route line, the routeLineBelowLayerId is specified to place
         // the route line below road labels layer on the map
         // the value of this option will depend on the style that you are using
@@ -392,7 +309,7 @@ class MapFragment : Fragment(R.layout.fragment_map) {
         routeArrowView = MapboxRouteArrowView(routeArrowOptions)
 
         // load map style
-        binding.mapView.mapboxMap.loadStyle(NavigationStyles.NAVIGATION_DAY_STYLE) {
+        binding.mapView.mapboxMap.loadStyle(NavigationStyles.NAVIGATION_NIGHT_STYLE) {
             // Ensure that the route line related layers are present before the route arrow
             routeLineView.initializeLayers(it)
 
@@ -433,9 +350,6 @@ class MapFragment : Fragment(R.layout.fragment_map) {
             }
         }
 
-        binding.stop.setOnClickListener {
-            clearRouteAndStopNavigation()
-        }
         binding.recenter.setOnClickListener {
             navigationCamera.requestNavigationCameraToFollowing()
             binding.routeOverview.showTextAndExtend(BUTTON_ANIMATION_DURATION)
@@ -444,20 +358,13 @@ class MapFragment : Fragment(R.layout.fragment_map) {
             navigationCamera.requestNavigationCameraToOverview()
             binding.recenter.showTextAndExtend(BUTTON_ANIMATION_DURATION)
         }
-        binding.soundButton.setOnClickListener {
-            // mute/unmute voice instructions
-            isVoiceInstructionsMuted = !isVoiceInstructionsMuted
-        }
-
-        // set initial sounds button state
-        binding.soundButton.unmute()
     }
 
     private fun initNavigation() {
-        MapboxNavigationApp.setup(
+        MapboxNavigationApp.setup {
             NavigationOptions.Builder(requireActivity())
                 .build()
-        )
+        }
 
         // initialize location puck
         binding.mapView.location.apply {
@@ -512,6 +419,7 @@ class MapFragment : Fragment(R.layout.fragment_map) {
                 .applyDefaultNavigationOptions()
                 .applyLanguageAndVoiceUnitOptions(requireActivity())
                 .coordinatesList(listOf(originPoint, destination))
+                .alternatives(true)
                 .apply {
                     // provide the bearing for the origin of the request to ensure
                     // that the returned route faces in the direction of the current user movement
@@ -554,29 +462,13 @@ class MapFragment : Fragment(R.layout.fragment_map) {
         mapboxNavigation.setNavigationRoutes(routes)
 
         // show UI elements
-        binding.soundButton.visibility = View.VISIBLE
         binding.routeOverview.visibility = View.VISIBLE
-        binding.tripProgressCard.visibility = View.VISIBLE
 
         // move the camera to overview when new route is available
         navigationCamera.requestNavigationCameraToOverview()
 
         // start simulation
         startSimulation(routes.first().directionsRoute)
-    }
-
-    private fun clearRouteAndStopNavigation() {
-        // clear
-        mapboxNavigation.setNavigationRoutes(listOf())
-
-        // stop simulation
-        stopSimulation()
-
-        // hide UI elements
-        binding.soundButton.visibility = View.INVISIBLE
-        binding.maneuverView.visibility = View.INVISIBLE
-        binding.routeOverview.visibility = View.INVISIBLE
-        binding.tripProgressCard.visibility = View.INVISIBLE
     }
 
     @OptIn(ExperimentalPreviewMapboxNavigationAPI::class)
@@ -587,12 +479,6 @@ class MapFragment : Fragment(R.layout.fragment_map) {
         mapboxNavigation.mapboxReplayer.pushEvents(replayData)
         mapboxNavigation.mapboxReplayer.seekTo(replayData[0])
         mapboxNavigation.mapboxReplayer.play()
-    }
-
-    @OptIn(ExperimentalPreviewMapboxNavigationAPI::class)
-    private fun stopSimulation() {
-        mapboxNavigation.mapboxReplayer.stop()
-        mapboxNavigation.mapboxReplayer.clearEvents()
     }
 
     companion object {
