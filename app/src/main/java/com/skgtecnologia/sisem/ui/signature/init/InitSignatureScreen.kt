f8bb01255f9d05f4e582dbb9926265330027321c
jvillad1 <jvillad1@gmail.com>
2024-10-10 23:55:25 -0500

    [fix][SMA-646]: Add Notifications to InitSignatureScreen.kt

--- app/src/main/java/com/skgtecnologia/sisem/ui/signature/init/InitSignatureScreen.kt
+++ app/src/main/java/com/skgtecnologia/sisem/ui/signature/init/InitSignatureScreen.kt
@@ -4,11 +4,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.skgtecnologia.sisem.commons.communication.NotificationEventHandler
 import com.skgtecnologia.sisem.domain.signature.model.SignatureIdentifier
 import com.skgtecnologia.sisem.ui.sections.BodySection
 import com.skgtecnologia.sisem.ui.sections.FooterSection
@@ -19,6 +24,8 @@ import com.valkiria.uicomponents.action.HeaderUiAction
 import com.valkiria.uicomponents.action.UiAction
 import com.valkiria.uicomponents.bricks.banner.OnBannerHandler
 import com.valkiria.uicomponents.bricks.loader.OnLoadingHandler
+import com.valkiria.uicomponents.bricks.notification.OnNotificationHandler
+import com.valkiria.uicomponents.bricks.notification.model.NotificationData
 import com.valkiria.uicomponents.components.textfield.InputUiModel
 import kotlinx.coroutines.launch
 import timber.log.Timber
@@ -31,6 +38,11 @@ fun InitSignatureScreen(
     onNavigation: (initSignatureNavigationModel: InitSignatureNavigationModel) -> Unit
 ) {
     val uiState = viewModel.uiState
+
+    var notificationData by remember { mutableStateOf<NotificationData?>(null) }
+    NotificationEventHandler.subscribeNotificationEvent {
+        notificationData = it
+    }
 
     LaunchedEffect(uiState) {
         launch {
@@ -86,6 +98,14 @@ fun InitSignatureScreen(
             ) { uiAction ->
                 handleFooterAction(uiAction, viewModel)
             }
+        }
+    }
+
+    OnNotificationHandler(notificationData) {
+        notificationData = null
+        if (it.isDismiss.not()) {
+            // TECH-DEBT: Navigate to MapScreen if is type INCIDENT_ASSIGNED
+            Timber.d("Navigate to MapScreen")
         }
     }
 
