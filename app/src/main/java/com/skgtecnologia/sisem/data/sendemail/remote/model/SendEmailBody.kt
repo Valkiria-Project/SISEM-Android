e64e876bdd8866b669bbc8b6739b63a1da86815b
jvillad1 <jvillad1@gmail.com>
2025-02-25 23:55:37 -0500

    [fix][SMA-710]: Send null body for Email

--- app/src/main/java/com/skgtecnologia/sisem/data/sendemail/remote/model/SendEmailBody.kt
+++ app/src/main/java/com/skgtecnologia/sisem/data/sendemail/remote/model/SendEmailBody.kt
@@ -5,5 +5,5 @@ import com.squareup.moshi.Json
 data class SendEmailBody(
     @Json(name = "id_aph") val idAph: String,
     @Json(name = "to") val to: String,
-    @Json(name = "body") val body: String
+    @Json(name = "body") val body: String?
 )
